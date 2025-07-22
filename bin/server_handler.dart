0428f3c5d238e6c50baf647b9483b9869b35b13f
Miguel-Galli <migueloliveira.oli@gmail.com>
2025-07-22 15:53:43 -0300

    Cria rotas get

--- bin/server_handler.dart
+++ bin/server_handler.dart
@@ -9,6 +9,20 @@ class ServerH {
       return Response(200, body: 'Primeira rota');
     });
 
+    //http://localhost:8080/ola/mundo/
+    //ola mundo
+
+    router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
+      return Response.ok("Olá, mundo $usuario");
+    });
+
+    //http://localhost:8080/ola/mundo/query?nome=miguel
+    router.get('/query', (Request req){
+      String? nome =req.url.queryParameters['nome'] ?? 'desconhecido';
+      String? idade =req.url.queryParameters['idade'] ?? 'desconhecido';
+      return Response.ok("Query eh: $nome, idade: $idade");
+    });
+
     return router;
   }
 }
